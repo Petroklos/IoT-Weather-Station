139ddcbd4ba096e307c6dc84a37af29d623f5770
Petroklos <38405504+Petroklos@users.noreply.github.com>
2023-03-28 21:01:55 +0300

    clean up

--- dataCollectorHybrid.ino
+++ dataCollectorHybrid.ino
@@ -1,34 +1,39 @@
-//hybrid analog/random value generator
+// Reads Analog Temperature value and Randomly Generates all other values
 
-float analogPin0 = A0, val, temp, airHum, groundHum, airInt;
+float analogPin0 = A0, value, temperature, airHumidity, groundHumidity, windIntensity;
 int r;
 
 void setup() {
-        Serial.begin(9600);
-        randomSeed(analogRead(0));
-        temp = 20.0;
-        airHum = 40.0;
-        groundHum = 50.0;
-        airInt = 10.0;
+    Serial.begin(9600);
+    randomSeed(analogRead(0));
+    temperature = 20.0;
+    airHumidity = 40.0;
+    groundHumidity = 50.0;
+    windIntensity = 10.0;
 }
 
 void loop() {
-        val = (analogRead(analogPin0)-511)/50;
-        temp = temp + temp*val/100;
+    value = (analogRead(analogPin0) - 511) / 50;
+    temperature += temperature * value / 100;
 
-        r = random(-10,10);
-        airHum = airHum + airHum*r/100;
+    r = random(-10, 10);
+    airHumidity += airHumidity * r / 100;
 
-        r = random(-10,10);
-        groundHum = groundHum + groundHum*r/100;
+    r = random(-10, 10);
+    groundHumidity += groundHumidity * r / 100;
 
-        r = random(-10,10);
-        airInt = airInt + airInt*r/100;
-        if (airInt < 0)
-                airInt = 0;
+    r = random(-10, 10);
+    windIntensity += windIntensity * r / 100;
+    if (windIntensity < 0) {
+        windIntensity = 0;
+    }
 
-        String toPrint = String(temp) + ' ' + String(airHum) + ' ' + String(groundHum) + ' ' + String(airInt);
-        Serial.println(toPrint);
+    // It's worth noting that airHum, groundHum and airInt values are randomly generated, while temp value is generated based on the analog sensor reading.
+    // The random value generated for each variable is a value between -10% and 10%, scaled by the current value of the variable. 
+    // This means that the simulated sensor readings will fluctuate randomly over time, but will generally trend upwards or downwards based on the initial value of the analog input pin.
+    
+    String toPrint = String(temperature) + ' ' + String(airHumidity) + ' ' + String(groundHumidity) + ' ' + String(windIntensity);
+    Serial.println(toPrint);
 
-        delay(3000);
+    delay(3000);
 }
